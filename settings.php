30267cd1fa2bca4453b87e3ef55f5bb01759ea14
Samuel Mortenson <samuel@mortenson.coffee>
2018-08-05 09:31:28 -0700

    Added initial content and settings.

--- settings.php
+++ settings.php
@@ -297,7 +297,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'qr2On-ERyqSfOUlNssOBIg50rvLzkic7811YW2SNmxmUcJpiQCylP_m6UaLSKlMTpQyPTL_I6Q';
 
 /**
  * Deployment identifier.
@@ -793,3 +793,9 @@ $databases['default']['default'] = array (
 );
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = '../config';
+$databases['default']['default'] = array (
+  'database' => 'sites/default/files/.ht.sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
+  'driver' => 'sqlite',
+);
