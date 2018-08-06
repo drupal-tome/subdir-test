826ac36c3c5b4de2df3a7b3895e2a3247dd4d8b0
Samuel Mortenson <samuel@mortenson.coffee>
2018-08-05 20:48:51 -0700

    Added gh-pages script.

---
+++ scripts/gh-pages.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+drush cr
+drush tome:static -l https://tome.fyi/subdir-test
+rm -rf gh-pages
+git clone git@github.com:drupal-tome/subdir-test.git -b gh-pages gh-pages
+rm -rf gh-pages/*
+cp -r html/subdir-test/* gh-pages/
+cd gh-pages
+git add .
+git commit -m 'Updating gh-pages site'
+git push -u origin gh-pages
