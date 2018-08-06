43c1522a6a91905fcdea634a55235e51a6c69852
Samuel Mortenson <samuel@mortenson.coffee>
2018-08-05 21:15:01 -0700

    Improved gh-pages script.

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+set -e
+
 drush cr
 drush tome:static -l https://tome.fyi/subdir-test
 rm -rf gh-pages
-git clone git@github.com:drupal-tome/subdir-test.git -b gh-pages gh-pages
+git clone git@github.com:drupal-tome/subdir-test.git gh-pages
+cd gh-pages
+git checkout -B gh-pages
+cd ..
 rm -rf gh-pages/*
 cp -r html/subdir-test/* gh-pages/
 cd gh-pages
